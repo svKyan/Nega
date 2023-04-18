5532646ad207e3d6f6a6213293ecd6831accf3ba
svKyan <kyan.bleeser0@gmail.com>
2023-04-18 22:17:08 +0200

    Haat

--- src/nega.c
+++ src/nega.c
@@ -3,6 +3,9 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+#define LEN(arr) (sizeof arr / sizeof arr[0])
+
+// TODO: Update this list and enable long flags
 const static char *usage =
 "Usage: nega <command> [options]\n"
 "Commands:\n"
@@ -20,60 +23,74 @@ const static char *usage =
 "    \x1b[35m-v/--verbose\x1b[0m\t\tVerbose output\n"
 ;
 
-static bool flag_yes = false;
-static bool flag_verbose = false;
+typedef struct {
+    char *name;
+    char *flags;
+    char **lflags;
+    bool *flag_status;
+    char *pacman_cmd;
+    char **pacman_flags;
+    unsigned int flagn;
+} Command;
 
-static bool flag_steal_refresh = false;
-static bool flag_steal_upgrade = false;
-static bool flag_steal_quiet = false;
-static bool flag_steal_wash = false;
+static char global_flags[] = { 'v' };
+static char *global_lflags[] = { "verbose" };
+static char *global_pacman_flags[] = { "v" };
+static bool global_status[LEN(global_flags)] = { 0 };
 
-static bool flag_yeet_cascase = false;
-static bool flag_yeet_recursive = false;
-static bool flag_yeet_unneeded = false;
+static char steal_flags[] = { 'r', 'u', 'q', 'w' };
+static char *steal_lflags[] = { "refresh", "upgrade", "quiet", "wash" };
+static char *steal_pacman_flags[] = { "yy", "u", "q", "c" };
+static bool steal_status[LEN(steal_flags)] = { 0 };
 
-static inline void check_is_valid_command(char *command) {
-    if (strcmp(command, "steal") != 0 && strcmp(command, "yeet") != 0) {
-        printf("\x1b[31mError: Unknown command '%s'\n\x1b[0m", command);
-        exit(1);
-    }
+static char yeet_flags[] = { 'c', 'r', 'u' };
+static char *yeet_lflags[] = { "cascase", "recursive", "unneeded" };
+static char *yeet_pacman_flags[] = { "c", "s", "u" };
+static bool yeet_status[LEN(yeet_flags)] = { 0 };
+
+static char query_flags[] = { 'c', 'd', 'e', 'g', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 's', 't', 'u' };
+static char *query_lflags[] = {
+    "changelog", "deps", "explicit", "groups", "info", "check", "list", "foreign", "native",
+    "owns", "file", "quiet", "search", "unrequired", "upgrades"
+};
+static char *query_pacman_flags[] = { "c", "d", "e", "g", "i", "k", "l", "m", "n", "o", "p", "q", "s", "t", "u" };
+static bool query_status[LEN(query_flags)] = { 0 };
+
+static Command commands[] = {
+    { "steal", steal_flags, steal_lflags, steal_status, "S", steal_pacman_flags, LEN(steal_flags) },
+    { "yeet", yeet_flags, yeet_lflags, yeet_status, "Rn", yeet_pacman_flags, LEN(yeet_flags) },
+    { "query", query_flags, query_lflags, query_status, "Q", query_pacman_flags, LEN(query_flags) },
+};
+static Command *chosen_command;
+
+static inline void check_is_valid_command(const char *command) {
+    for (int i = 0; i < LEN(commands); ++i)
+        if (strcmp(command, commands[i].name) == 0) {
+            chosen_command = &commands[i];
+            return;
+        }
+    
+    printf("\x1b[31mError: Unknown command '%s'\n\x1b[0m", command);
+    exit(1);
 }
 
-static inline void grab_steal_flags(char flag) {
-    switch (flag) {
-        case 'r':
-            flag_steal_refresh = true;
-            break;
-        case 'u':
-            flag_steal_upgrade = true;
-            break;
-        case 'q':
-            flag_steal_quiet = true;
-            break;
-        case 'w':
-            flag_steal_wash = true;
-            break;
-        default:
-            printf("\x1b[31mError: Unknown flag '%c'\n\x1b[0m", flag);
-            exit(1);
-    }
+static bool apply_command_flag(Command *command, char flag) {
+    for (int i = 0; i < command->flagn; ++i)
+        if (flag == command->flags[i])
+            return command->flag_status[i] = true;
+    
+    return false;
 }
 
-static inline void grab_yeet_flags(char flag) {
-    switch (flag) {
-        case 'c':
-            flag_yeet_cascase = true;
-            break;
-        case 'r':
-            flag_yeet_recursive = true;
-            break;
-        case 'u':
-            flag_yeet_unneeded = true;
-            break;
-        default:
-            printf("\x1b[31mError: Unknown flag '%c'\n\x1b[0m", flag);
-            exit(1);
-    }
+static bool apply_flag(char flag) {
+    for (int i = 0; i < LEN(global_flags); ++i)
+        if (flag == global_flags[i])
+            return global_status[i] = true;
+
+    if (apply_command_flag(chosen_command, flag))
+        return true;
+
+    return false;
 }
 
 int main(int argc, char *argv[]) {
@@ -82,27 +99,17 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    char *command = argv[1];
+    const char *command = argv[1];
     check_is_valid_command(command);
-    int entry_num = -1; // From which index the subjects in argv start, TODO: Cannot handle multiple subjects
+    int entry_num = -1;
 
     for (int i = 2; i < argc; ++i) {
         if (argv[i][0] == '-') {
-            for (int j = 1; argv[i][j] != '\0'; ++j) {
-                switch (argv[i][j]) {
-                    case 'y':
-                        flag_yes = true;
-                        break;
-                    case 'v':
-                        flag_verbose = true;
-                        break;
-                    default:
-                        if (strcmp(command, "steal") == 0)
-                            grab_steal_flags(argv[i][j]);
-                        else if (strcmp(command, "yeet") == 0)
-                            grab_yeet_flags(argv[i][j]);
+            for (int j = 1; argv[i][j] != '\0'; ++j)
+                if (!apply_flag(argv[i][j])) {
+                    printf("\x1b[31mError: Unknown flag '%c'\n\x1b[0m", argv[i][j]);
+                    return 1;
                 }
-            }
         } else {
             entry_num = i;
             break;
@@ -110,30 +117,19 @@ int main(int argc, char *argv[]) {
     }
 
     int status;
-    char *com;
+    char com[32] = "pacman -";
+    strcat(com, chosen_command->pacman_cmd);
+    for (int i = 0; i < LEN(global_flags); ++i)
+        if (global_status[i])
+            strcat(com, global_pacman_flags[i]);
+    for (int i = 0; i < chosen_command->flagn; ++i)
+        if (chosen_command->flag_status[i])
+            strcat(com, chosen_command->pacman_flags[i]);
+    strcat(com, " ");
+    if (entry_num != -1)
+        strcat(com, argv[entry_num]);
 
-    if (strcmp(command, "steal") == 0) {
-        snprintf(com, 32, "pacman -S%s%s%s%s %s",
-            flag_steal_refresh ? "yy" : "",
-            flag_steal_upgrade ? "u" : "",
-            flag_steal_quiet ? "q" : "",
-            flag_steal_wash ? "c" : "",
-            entry_num == -1 ? "" : argv[entry_num]
-        );
-    } else if (strcmp(command, "yeet") == 0) {
-        snprintf(com, 32, "pacman -Rn%s%s%s %s",
-            flag_yeet_cascase ? "c" : "",
-            flag_yeet_recursive ? "s" : "",
-            flag_yeet_unneeded ? "u" : "",
-            entry_num == -1 ? "" : argv[entry_num]
-        );
-    }
-
-    status = system(com);
-
-    if (status != 0) {
-        return 1;
-    }
+    printf("Executing %s", com);
 
     return 0;
 }
